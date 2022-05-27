2504f23848629d199d509692b731462f8d0214a9
bowe-m <diseasemodern@gmail.com>
2022-05-26 22:10:34 -0400

    trying to fix github pages site, maybe files were uncapitialized so it didn't work?

--- scene/PreloaderScene.js
+++ scene/PreloaderScene.js
@@ -41,10 +41,10 @@ class PreloaderScene extends Phaser.Scene{
     this.load.image('closet','assets/Closet.png');
     this.load.image('desk','assets/Desk.png');
     this.load.image('window','assets/Window.png');
-    this.load.spritesheet('candle', 'assets/candle.png', {frameWidth: 32, frameWidth: 32});
+    this.load.spritesheet('candle', 'assets/Candle.png', {frameWidth: 32, frameWidth: 32});
     this.load.image('toaster', 'assets/Toaster.png');
     this.load.image('cord', 'assets/extensionCord.png');
-    this.load.image('lamp', 'assets/lamp.png');
+    this.load.image('lamp', 'assets/Lamp.png');
     this.load.image('extinguisher', 'assets/fireExtinguisher.png');
 
 
